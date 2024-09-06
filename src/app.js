a6115d0e66814c1eab4fd0de3d5036490b97da36
erikcavalcante <erikcavalcante94@gmail.com>
2024-09-06 01:07:45 -0300

    Atualiza arquivos de configuração para autopilot-cluster-2

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 4000; // Porta configurável através de variáveis de ambiente
+const port = 4000; // Porta configurável através de variáveis de ambiente
 
 app.use(bodyParser.json());
 app.use('/api/v1', routes); // Usa o arquivo de rotas para definir os endpoints
